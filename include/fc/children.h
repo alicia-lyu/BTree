46bbdfdccda72766fdeb10f93dc0d65ff87f9fe0
alicia-lyu <wlyu27@wisc.edu>
2024-07-11 13:59:03 -0500

    Satisfy viewable range

--- include/fc/children.h
+++ include/fc/children.h
@@ -2,9 +2,12 @@
 #define CHILDREN_H
 
 #include <array>
+#include <concepts>
 #include <cstddef>
 #include <iterator>
 #include <memory>
+#include <variant>
+#include <vector>
 #include "fc/details.h"
 
 namespace children {
@@ -35,6 +38,8 @@ struct ChildrenIterTraits {
   using pointer = value_type *;
   using reference = value_type &;
   using iterator_category = std::bidirectional_iterator_tag;
+  using iterator_concept = iterator_category;
+
   using nodes_iter = typename children_nodes_type<Node, Deleter>::iterator;
   using data_iter = typename children_data_type<disk_max_nkeys>::iterator;
 };
@@ -46,6 +51,8 @@ struct ChildrenConstIterTraits {
   using pointer = const value_type *;
   using reference = const value_type &;
   using iterator_category = std::bidirectional_iterator_tag;
+  using iterator_concept = iterator_category;
+
   using nodes_iter = typename children_nodes_type<Node, Deleter>::const_iterator;
   using data_iter = typename children_data_type<disk_max_nkeys>::const_iterator;
 };
@@ -58,34 +65,38 @@ class ChildrenIterator {
   using pointer = typename IterTraits::pointer;
   using reference = typename IterTraits::reference;
   using iterator_category = typename IterTraits::iterator_category;
+  using iterator_concept = typename IterTraits::iterator_concept;
 
  private:
   using nodes_iter = typename IterTraits::nodes_iter;
   using data_iter = typename IterTraits::data_iter;
   std::variant<nodes_iter, data_iter> iter_;
 
+ public:
   explicit ChildrenIterator(std::variant<nodes_iter, data_iter> iter) : iter_(iter) {}
 
-  explicit ChildrenIterator(nodes_iter iter) : iter_(iter) {}
-
-  explicit ChildrenIterator(data_iter iter) : iter_(iter) {}
+  ChildrenIterator() {
+    iter_ = nodes_iter();
+  }
 
   ChildrenIterator &operator++() {
-    std::visit([](auto &i) { ++i; }, iter_);
-    return *this;
+    return std::visit([](auto &i) { return ++i; }, iter_);
+  }
+
+  ChildrenIterator operator++(int) {
+    return std::visit([](auto &i) { return i++; }, iter_);
   }
 
   ChildrenIterator &operator--() {
-    std::visit([](auto &i) { --i; }, iter_);
-    return *this;
+    return std::visit([](auto &i) { return --i; }, iter_);
   }
 
-  value_type &operator*() {
-    return std::visit([](auto &i) -> value_type & { return *i; }, iter_);
+  reference operator*() {
+    return std::visit([](auto &i) -> reference { return *i; }, iter_);
   }
 
-  value_type *operator->() {
-    return std::visit([](auto &i) -> value_type * { return &*i; }, iter_);
+  pointer operator->() {
+    return std::visit([](auto &i) -> pointer { return &*i; }, iter_);
   }
 
   bool operator==(const ChildrenIterator &other) const { return iter_ == other.iter_; }
@@ -94,45 +105,51 @@ class ChildrenIterator {
 };
 
 template <class Node, class Deleter, attr_t disk_max_nkeys>
-class Children : public std::variant<children_nodes_type<Node, Deleter>, children_data_type<disk_max_nkeys>> {
-
+class Children {
   using nodes_type = children_nodes_type<Node, Deleter>;
   using data_type = children_data_type<disk_max_nkeys>;
   using child_type = Child<Node, Deleter>;
 
+  std::variant<nodes_type, data_type> data_;
+
  public:
   using iterator = ChildrenIterator<ChildrenIterTraits<Node, Deleter, disk_max_nkeys>>;
   using const_iterator = ChildrenIterator<ChildrenConstIterTraits<Node, Deleter, disk_max_nkeys>>;
+  using value_type = typename iterator::value_type;
+
+  Children() {
+    data_ = nodes_type();
+  }
 
   void reserve(size_t n) {
-    if (std::holds_alternative<nodes_type>(*this)) {
-      std::get<nodes_type>(*this).reserve(n);
+    if (std::holds_alternative<nodes_type>(data_)) {
+      std::get<nodes_type>(data_).reserve(n);
     } else {
       throw std::runtime_error("Cannot reserve for children_data_type");
     }
   }
 
   void push_back(std::unique_ptr<Node, Deleter> &&node) {
-    if (std::holds_alternative<nodes_type>(*this)) {
-      std::get<nodes_type>(*this).push_back(std::move(node));
+    if (std::holds_alternative<nodes_type>(data_)) {
+      std::get<nodes_type>(data_).push_back(std::move(node));
     } else {
       throw std::runtime_error("Cannot push_back for children_data_type");
     }
   }
 
   child_type &operator[](size_t idx) {
-    if (std::holds_alternative<nodes_type>(*this)) {
-      return std::get<nodes_type>(*this)[idx];
+    if (std::holds_alternative<nodes_type>(data_)) {
+      return std::get<nodes_type>(data_)[idx];
     } else {
-      return std::get<data_type>(*this)[idx];
+      return std::get<data_type>(data_)[idx];
     }
   }
 
   bool empty() const {
-    if (std::holds_alternative<nodes_type>(*this)) {
-      return std::get<nodes_type>(*this).empty();
+    if (std::holds_alternative<nodes_type>(data_)) {
+      return std::get<nodes_type>(data_).empty();
     } else {
-      for (auto &&child : std::get<data_type>(*this)) {
+      for (auto &&child : std::get<data_type>(data_)) {
         if (child != 0) {
           return false;
         }
@@ -141,23 +158,27 @@ class Children : public std::variant<children_nodes_type<Node, Deleter>, childre
     }
   }
 
-  // For std::ranges::move
-  iterator begin() {
-    return std::visit([](auto &children) { return iterator(children.begin()); }, *this);
+  iterator begin() noexcept {
+    return std::visit([](auto &children) { return iterator(children.begin()); }, data_);
   }
 
-  iterator end() {
-    return std::visit([](auto &children) { return iterator(children.end()); }, *this);
+  iterator end() noexcept {
+    return std::visit([](auto &children) { return iterator(children.end()); }, data_);
   }
 
-  const_iterator begin() const {
-    return std::visit([](auto &children) { return const_iterator(children.cbegin()); }, *this);
+  const_iterator begin() const noexcept {
+    return std::visit([](const auto &children) { return const_iterator(children.cbegin()); }, data_);
   }
 
-  const_iterator end() const {
-    return std::visit([](auto &children) { return const_iterator(children.cend()); }, *this);
+  const_iterator end() const noexcept {
+    return std::visit([](const auto &children) { return const_iterator(children.cend()); }, data_);
   }
+
+  const_iterator cbegin() const noexcept { return begin(); }
+
+  const_iterator cend() const noexcept { return end(); }
 };
+
 }  // namespace children
 
 #endif
